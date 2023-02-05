ac057f347fa0878a9c9ae952b515a8001ee6c7dc
Nnamani John <nnamanijohn123@gmail.com>
2023-02-06 00:05:35 +0100

    input output

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Product} from "../../products/product";
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
+  @Input() product: Product = {
+    id: 0,
+    name: '',
+    price: 0,
+    url: '',
+    description: ''
+  };
+  @Input() btnShow = true;
 
+  @Output() add = new EventEmitter();
+
+  addToCart() {
+    this.add.emit()
+  }
 }
